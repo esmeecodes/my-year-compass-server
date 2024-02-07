1d5e496ba4f557a1739199b21500b3c02f9043ac
Esmée Zootjes <esmeezootjes@gmail.com>
2024-02-07 19:14:57 +0100

    changed the model to do a simple get/put route first

--- routes/compass.routes.js
+++ routes/compass.routes.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 
 const User = require("../models/User.model");
 const Compass = require("../models/Compass.model");
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
+// route to create the compass
 router.post("/mycompasses", async (req, res, next) => {
   try {
     const { compassTitle, userId } = req.body;
@@ -15,16 +17,33 @@ router.post("/mycompasses", async (req, res, next) => {
     await User.findByIdAndUpdate(userId, {
       $push: { compasses: newCompass._id },
     });
-    await Compass.findByIdAndUpdate(newCompass._id, {
-      $push: { user: userId },
-    });
+
     res.json(newCompass);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+// route to show all the created compassess in the my-compass page
+router.get("/mycompasses/:userId", async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    const allCompasses = await User.findById(userId).populate("compasses");
+    res.json(allCompasses);
+  } catch (e) {
+    res.json(e);
+  }
+});
 
-router.get("/mycompasses/:userId", async (req, res, next) => {});
+// route to show the compass in the editing page
+router.get("/compass/overview/:compassId", async (req, res, next) => {
+  const { compassId } = req.params;
+  try {
+    const oneCompassToEdit = await Compass.findById(compassId);
+    res.json(oneCompassToEdit);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 module.exports = router;
